06bf2fec25ffc3d66f6c6c3eb5738ba8c21ce3e1
Alexey <akurzov41@gmail.com>
2025-05-01 17:42:01 +0000

    Added trim function in utils

---
+++ utils/string/trim.h
@@ -0,0 +1,10 @@
+#include <string>
+
+namespace cxx {
+
+    void ltrim(std::string & s);
+    void rtrim(std::string & s);
+    std::string ltrimCopy(std::string s);
+    std::string rtrimCopy(std::string s);
+
+} // namespace cxx
